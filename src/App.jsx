da434010ac14050bfd0d22b9537786ab39441f1d
Audri12 <98655796+Audri12@users.noreply.github.com>
2024-09-16 20:43:18 +0600

    draft

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import AllMember from "./Component/Page/Our Team/AllMember"
+import AllMembersDraft from "./Component/Page/Our Team/AllMembersDraft"
+
+function App() {
+  
+  return (
+    <>
+    <div>
+      <AllMember></AllMember>
+      <AllMembersDraft></AllMembersDraft>
+    </div>
+    </>
+  )
+}
+
+export default App
